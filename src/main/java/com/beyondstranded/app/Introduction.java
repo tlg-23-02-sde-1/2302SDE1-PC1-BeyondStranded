6ecfb556756cc7d7cb7811b9811d2f8d7889be8f
Christopher Baker <christopher.baker@tlgcohort.com>
2023-05-18 12:39:43 -0700

    Commit branch AB#14 with player title/splash screen, AB#14

--- src/main/java/com/beyondstranded/app/Introduction.java
+++ src/main/java/com/beyondstranded/app/Introduction.java
@@ -10,15 +10,24 @@ import static com.util.apps.Console.*;
 
 class Introduction {
 
+    private static String banner;
     private static String objective;
     private static String welcome;
     private static String newGame;
     private static String story;
 
+
     private final Prompter prompter;
 
     Introduction(Prompter prompter) {
         this.prompter = prompter;
+    }
+
+    void showTitlePage() {
+        blankLines(1);
+        clear();
+        System.out.println(banner);
+        prompter.prompt("\n\t\t\t\t\tPress Enter to Continue:","","\t\t\t\t\tInvalid input. Only press Enter in your keyboard.\n");
     }
 
     void gameOption() {
@@ -43,6 +52,7 @@ class Introduction {
 
     static {
         try {
+            banner = Files.readString(Path.of("src/main/resources/ASCII_Art/Banner.txt"));
             newGame = Files.readString(Path.of("src/main/resources/images/New Game Prompt.txt"));
             story = Files.readString(Path.of("src/main/resources/images/Storyline Prompt.txt"));
             objective = Files.readString(Path.of("src/main/resources/images/Objective Prompt.txt"));
