79a6acec72665ca3cd1eac35c882c6a6333c4872
Christopher Baker <christopher.baker@tlgcohort.com>
2023-05-19 10:27:59 -0700

    update to, AB#17

--- src/main/java/com/beyondstranded/app/Parser.java
+++ src/main/java/com/beyondstranded/app/Parser.java
@@ -53,7 +53,7 @@ class Parser {
 
             if (input.equals("quit")) {
                 userInput.add(0,"quit");
-                validInput = true;
+                break;
             }
             else if(input.equals("")) {
                 System.out.printf("\nInvalid Input. Input is empty. Input: %s", input);
