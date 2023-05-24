6b5b5592ada3e66f2cfbc24eac7a27573e90c060
Jessica G <91770301+Gi-bi@users.noreply.github.com>
2023-05-24 13:29:14 -0700

    unit test completion, AB#19

---
+++ src/test/java/com/beyondstranded/app/JsonModTest.java
@@ -0,0 +1,15 @@
+package com.beyondstranded.app;
+
+import com.google.gson.stream.JsonReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import static com.google.gson.stream.JsonReader.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class JsonModTest {
+
+}
